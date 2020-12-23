d3a0a622b71c00d2e9f7eadd35b19bc3bfe468b6
Esteban Indiveri <esteban.indiveri@gmail.com>
2020-12-22 21:14:30 -0300

    add env vars

--- config/email.js
+++ config/email.js
@@ -4,10 +4,7 @@ dotenv.config({path: '.env'})
 
 module.exports={
     user: `${process.env.USER_MAIL}`,
-    // user: "1abdd1ee324289",
     pass: `${process.env.USER_PASS}`,
-    // pass: "745e736fa05400",
-    // host: "smtp.mailtrap.io",
     host: `${process.env.USER_HOST}`,
 
     port: 587,
